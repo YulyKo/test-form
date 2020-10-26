8e703503373e8dafc20613926a1219c35811a407
YulyKo <juliakotenko.forstudy@gmail.com>
2020-10-26 11:09:11 +0300

    set base stucture

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button-add />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import AddNewCourseButton from '@/components/AddNewCourseButton.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    'button-add': AddNewCourseButton,
   },
 };
 </script>
