8ce711d1d4e8bed18d44cf43371c0a413f7e327f
YulyKo <juliakotenko.forstudy@gmail.com>
2020-10-27 11:08:55 +0200

    changed styles in button +

--- src/components/AddNewCourseButton.vue
+++ src/components/AddNewCourseButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button class="text title button" @click="switchVisibilityModalForm">
+    <button class="text button" @click="switchVisibilityModalForm">
         +
     </button>
     <section v-show="visibleForm">
@@ -33,23 +33,17 @@ export default {
 <style lang="sass" scoped>
 @import '../main.sass'
 
-@mixin transition($args...)
-  -webkit-transition: $args
-  -moz-transition: $args
-  -o-transition: $args
-  transition: $args
-
-.button
-  background: $white
-  height: 12em
-  width: 12em
-  border-radius: 10px
-  border: none
-  cursor: pointer
-  @include transition(all .3s cubic-bezier(0.4, 0, 1, 1))
-  box-shadow: 0 0 41px -11px $gray
-  &:hover
-    background-color: $green
-    box-shadow: inset 0 0 15px -11px $green
-    color: $white
+// .button
+//   background: $white
+//   height: 12em
+//   width: 12em
+//   border-radius: 10px
+//   border: none
+//   cursor: pointer
+//   @include transition(all .3s cubic-bezier(0.4, 0, 1, 1))
+//   box-shadow: 0 0 41px -11px $gray
+//   &:hover
+//     background-color: $green
+//     box-shadow: inset 0 0 15px -11px $green
+//     color: $white
 </style>
